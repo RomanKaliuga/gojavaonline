b6adfec802bfa99faa72ec8d4bb9a9f7091d60f5
RomanKaliuga <mxpa@ukr.net>
2016-05-27 14:38:09 +0300

    Add files via upload

--- firstprogram.java
+++ firstprogram.java
@@ -1,5 +1,5 @@
 public class FirstProgram {
- public static void main(String[] args){
-	System.out.println("\"Hi, I'm your first Java program! Stay tuned!\"");
- }	
+    public static void main(String[] args){
+	    System.out.println("\"Hi, I'm your first Java program! Stay tuned!\"");
+    }
 }
